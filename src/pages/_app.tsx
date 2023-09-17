92b8684a7117a8a341e36d64dc8a3c530fb4d57e
Andrey Simonov <sasmeo@gmail.com>
2023-09-17 15:24:14 +0600

    second step

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,13 @@ const MyApp: AppType<{ session: Session | null }> = ({
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider 
+      session={session}
+      // Re-fetch session every 5 minutes
+      // refetchInterval={5 * 60}
+      // Re-fetches session when window is focused
+      // refetchOnWindowFocus={true}
+    >
       <Component {...pageProps} />
     </SessionProvider>
   );
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,7 +5,7 @@ import {
   type DefaultSession,
   type NextAuthOptions,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
@@ -48,21 +48,67 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: "Credentials",
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const res = await fetch("/api/login",{
+          method:"POST",
+          headers:{
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: credentials?.username,
+            password: credentials?.password,
+          })
+        })
+
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const user = await res.json();
+        
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      }
+    })
   ],
+  // callbacks: {
+  //   jwt: ({token, user}) => {
+  //     if(user){
+  //       token.id = user.id;
+  //     }
+  //     return token;
+  //   },
+  //   session: ({session, token}) => {
+  //     if(token){
+  //       session.id = token.id;
+  //     }
+  //     return session;
+  //   },
+  // },
+  // secret: "test",
+  // jwt: {
+  //   secret: "test",
+  // }
 };
+
+// export { handler as GET, hadnler as POST };
 
 /**
  * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
